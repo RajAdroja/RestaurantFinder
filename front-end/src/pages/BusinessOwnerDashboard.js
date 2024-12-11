56d4fb39464779e5bd3f04ec4d16d4c4a491858d
Surya Prakash <suryasanny001@gmail.com>
2024-12-10 17:24:30 -0800

    BOchanges ---

--- front-end/src/pages/BusinessOwnerDashboard.js
+++ front-end/src/pages/BusinessOwnerDashboard.js
@@ -31,12 +31,18 @@ const BusinessOwnerDashboard = ({ listings, updateListing, deleteListing, addLis
                     Authorization: `Bearer ${token}`  // Include the token for authentication
                 }
             });
+            console.log(response.data);
             setRestaurantListings(response.data); // Update state with the fetched data
         } catch (error) {
             console.error("Error fetching restaurant listings:", error);
         }
     };
+    
+    useEffect(() => {
+        fetchListings();
+    }, []); // Empty array ensures this runs only once when the component mounts
 
+    console.log(restaurantListings.length);
     const handleAddListingChange = (e) => {
         const { name, value, files } = e.target;
         if (files) {
@@ -100,13 +106,13 @@ const BusinessOwnerDashboard = ({ listings, updateListing, deleteListing, addLis
             console.error("Error updating restaurant:", error);
         }
     };
-
+    
     const viewRestaurantDetails = (restaurantId) => {
         navigate(`/restaurant/${restaurantId}`);
     };
 
     return (
-        <div className="p-6">
+       <div className="p-6">
             <h1 className="text-4xl font-bold mb-4">Business Owner Dashboard</h1>
 
             <button
